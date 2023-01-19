f17611f82606663e4de6d56886d7884f830dd018
YoungSan14 <suarezsan14@gmail.com>
2023-01-19 16:46:47 -0300

    test 10/10

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -14,10 +14,25 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if (n === 1){
+    return n;
+  }else{
+    return n * nFactorial(n - 1);
+  }
+}
+// console.log(nFactorial(3));
+// console.log(nFactorial(4));
+// console.log(nFactorial(5));
 
-function nFibonacci(n) {}
-
+function nFibonacci(n) {
+  if (n <= 1){
+    return n;
+  }else{
+    return nFibonacci(n - 1) + nFibonacci(n - 2);
+  }
+}
+// console.log(nFibonacci(3))
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
   - enqueue: agrega un valor respetando el orden.
@@ -27,11 +42,40 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+function Queue() {
+  this.queue = []
 
+  this.enqueue = function(ingresa){
+    this.queue.push(ingresa);
+  }
+  this.dequeue = function(){
+    // (this.queue.length === 0) ? undefined : this.queue.shift();  
+    if (this.queue.length === 0){
+      return undefined;
+    }else{
+      return this.queue.shift();
+    }
+  }
+  this.size = function(){
+    return this.queue.length;
+  }
+}
+// Queue.prototype.enqueue = function (ingresa){
+//   this.queue.push(ingresa);
+// }
+let cola = new Queue;
+cola.enqueue(1);
+cola.enqueue(2);
+cola.enqueue(3);
+// cola.dequeue();
+// cola.dequeue();
+// cola.dequeue();
+// cola.dequeue();
+cola.size();
+// console.log(cola)
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
-   Queue,
-   nFactorial,
-   nFibonacci,
+  Queue,
+  nFactorial,
+  nFibonacci,
 };
