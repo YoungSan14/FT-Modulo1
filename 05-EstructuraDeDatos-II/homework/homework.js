a29454308122d14f90953bffd86b13a9783384a7
YoungSan14 <suarezsan14@gmail.com>
2023-01-23 14:03:32 -0300

    test 17/20

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -10,9 +10,89 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
-function LinkedList() {}
+function Node(dato){
+  this.dato = dato;
+  this.next = null;
+} 
 
-function Node(value) {}
+function LinkedList() {
+  this.length = 0; 
+  this.head = null;
+
+  this.add = function(dato){
+    let nuevoNodo = new Node(dato);
+    let actual = this.head;
+
+    if(actual === null){
+      this.head = nuevoNodo;
+      this.length++;
+      return nuevoNodo;
+    }
+
+    while(actual.next !== null){
+      actual = actual.next;
+    }
+
+    actual.next = nuevoNodo;
+    this.length++
+    return nuevoNodo;
+  }
+
+  this.remove = function(){
+    let dato = null;
+    let actual = this.head;
+    
+    if (this.length === 1){
+      dato = actual.dato;
+      this.head = null;
+      this.length--;
+      return dato;
+    }else if(this.length > 1){  
+      dato = actual.next.dato;
+      while(actual.next.next !== null){
+        actual = actual.next;
+        dato = actual.next.dato;
+      }
+      actual.next = null;
+      this.length--;
+      return dato;
+    }else{
+      return null
+    }
+  }
+
+  this.search = function(dato){
+    if (!this.head) return null;
+    let actual = this.head;
+    while (actual) {
+      if (actual.dato === dato) return actual.dato;
+      else if (typeof dato === "function") { 
+        if (dato(actual.dato)) return actual.dato;
+      }
+      actual = actual.next;
+    }
+    return null;
+  }
+}
+// this.search = function(dato){
+//   // if(dato()){
+//   let actual = this.head;
+//   if (actual.dato === dato){
+//     return actual.dato;
+//   }
+
+//   while(actual.next !== null){
+//     if(actual.dato === dato) return actual.dato; 
+//     else if(typeof dato === 'function'){
+//       if(dato(actual.dato)) return actual.dato;
+//     }
+//     actual = actual.next;  
+//     // if(actual.dato === dato){
+//     // return actual.dato;
+//     // }
+//   }
+//   return null;
+//   }
 
 /* EJERCICIO 2
 Implementar la clase HashTable.
@@ -27,8 +107,54 @@ La clase debe tener los siguientes métodos:
 
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
-function HashTable() {}
+function HashTable() {
+  this.espacio = 35;
+  this.coleccion = [];
+  // {instructora: 'Ani'}, {saludo: 'Hola'}
+  this.hash = function(string){
+    let strArray = string.split('');
+    let valoresNumericos = strArray.map(valor => valor.charCodeAt());
+    let sumaDeLosValores = valoresNumericos.reduce((acum, valor) => (acum + valor));
+  
+    return sumaDeLosValores % this.espacio;
+  }
 
+  this.set = function(clave, valor){
+    let posicion = this.hash(clave);
+    if(!this.coleccion[posicion]){
+      this.coleccion[posicion] = {};
+    }
+    this.coleccion[posicion][clave] = valor;
+  }
+
+  this.get = function(clave){
+    let posicion = this.hash(clave);
+    return this.coleccion[posicion][clave];
+  }
+
+  this.hasKey = function(clave){
+    return this.coleccion.some(objeto => {
+      if(objeto.hasOwnProperty(clave)) return true;
+        return false;
+    });
+  }
+}
+
+let casilleros = new HashTable();
+// console.log(casilleros.hash('foo'));
+// console.log(casilleros.hasKey('saludo'));
+// casilleros.set('foo', 'bar1')
+// console.log(casilleros.get('foo'))
+// casilleros.set('12', 'dose')
+// casilleros.set('1', 'uno')
+// console.log(casilleros.coleccion[9])
+
+// console.log('1Alo1S'.split(''));
+// console.log('hola'.charCodeAt(3));
+// let array = [];
+
+// array[12] = 'hola';
+// console.log(array) 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
