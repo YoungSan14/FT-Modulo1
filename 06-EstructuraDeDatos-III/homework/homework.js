0b33febfd2e065cee008fcd1e3d9644f5d26fd75
YoungSan14 <suarezsan14@gmail.com>
2023-01-26 23:45:20 -0300

    test 7/10

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -9,40 +9,143 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-
-
-function BinarySearchTree() {
-   this.root = null;
-   this._length = 0;
-
-   this.size = function(){
-      return this._length;
+/*                CODIGO EN ESPAÑOL 
+class BinarySearchTree{
+   constructor(data, izquierda = null, derecha = null){
+      this.data = data,
+      this.izquierda = izquierda,
+      this.derecha = derecha
    }
 
-   this.insert = function(valor){
-      let nodo =  {
-         valor: valor,
-         izquierda: null,
-         derecha: null
-      }
-      if(this.root === null) this.root = nodo;
-      else if(nodo.valor < this.root.valor && !this.root.izquierda){
-
-      }
+   size(){
+      return cont();
    }
 
-   this.contains = function(valor){
-      function seEncuentra(valor, nodo){
-         // if(nodo.valor === valor)
+   insert(data){
+      if (this.data > data){
+         if(this.izquierda !== null){
+            this.izquierda.insert(data);   
+         }else{
+            this.izquierda = new BinarySearchTree(data); 
+            cont();
+         }
+      }
+      if(this.data < data){
+         if(this.derecha !== null){
+            this.derecha.insert(data);   
+         }else{
+            this.derecha = new BinarySearchTree(data);
+            cont();
+         }
       }
    }
-
-   this.depthFirstForEach = function(){}
 }
-// let arbol = new BinarySearchTree()
-// arbol.insert(10)
-// console.log(arbol)
-// arbol.contains(3)
+*/
+
+class BinarySearchTree{
+   constructor(value, left = null, right = null){
+      this.value = value,
+      this.left = left,
+      this.right = right
+   }
+
+   size(){
+      if(this.left === null && this.right === null) return 1;
+      else if(this.left === null && this.right) return 1 + this.right.size();
+      else if(this.right === null && this.left) return 1 + this.left.size();
+      else if(this.left && this.right) return 1 + this.left.size() + this.right.size();     
+   }
+
+   insert(value){
+      if (this.value > value){
+         if(this.left !== null){
+            this.left.insert(value);   
+         }else{
+            this.left = new BinarySearchTree(value); 
+            // contador++;
+            // cont();
+         }
+      }
+      if(this.value < value){
+         if(this.right !== null){
+            this.right.insert(value);   
+         }else{
+            this.right = new BinarySearchTree(value);
+            // contador++;
+            // cont();
+         }
+      }
+   }
+
+   contains(value){
+      if(value === this.value) return true;
+      if(value < this.value){
+         if(this.left === null){
+            return false;
+         }else{
+            return this.left.contains(value);
+         }
+      }else{
+         if(this.right === null){
+            return false;
+         }else{
+            return this.right.contains(value);
+         }
+      }
+   }
+
+   depthFirstForEach(orden = "in-order"){}
+
+   breadthFirstForEach(callback, array = []){
+      if (this.left !== null){
+         array.push(this.left);
+      }
+      if (this.right !== null){
+         array.push(this.right);
+      }
+
+      callback(this.value)
+      
+      if(array.length > 0){
+         let elemento = array.shift();
+         elemento.breadthFirstForEach(callback, array);
+      }
+   }
+}
+
+// function contador(){
+//    let count = 1;
+//    return function(){
+//       return count++;
+//    }
+// }
+// let cont = contador(); 
+let contador = 0;
+let arbol = new BinarySearchTree(20);
+arbol.insert(15)
+arbol.insert(25)
+arbol.insert(5)
+arbol.insert(17)
+arbol.insert(21)
+arbol.insert(28)
+arbol.insert(0)
+console.log(arbol.size())
+console.log(arbol.contains(20))
+
+
+/*
+arbol.insert(14);
+arbol.insert(50);
+arbol.insert(1);
+arbol.insert(45);
+arbol.insert(13);
+arbol.insert(12);
+arbol.insert(11);
+console.log(arbol.izquierda.izquierda.derecha.izquierda.izquierda.izquierda.data);
+// console.log(arbol.izquierda.izquierda.derecha)
+console.log(arbol)
+arbol.contains(3)
+*/
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
